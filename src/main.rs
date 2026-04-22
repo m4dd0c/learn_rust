74543e9e32af2fae984a3965fd42207d8059d704
Manish Kumar <77256586+m4dd0c@users.noreply.github.com>
2026-04-22 07:39:18 +0000

    fix: handled for input 1, and 2

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,32 @@
 use std::io;
 
 fn main() {
+    fibonacci_til_n();
+
+    println!("Program ended");
+}
+
+fn fibonacci_til_n() {
     // asking for the number of elems from user
-    println!("Enter number of elements you need");
+    println!("Enter number of elements you need: ");
     let mut num = String::new();
     io::stdin().read_line(&mut num).expect("Expected string");
     let num: u32 = num.trim().parse().expect("Not a Number");
     println!("Printing {num} Elements of fibonacci series...");
     fibonacci(num);
-    println!("Program ended");
 }
 
-fn fibonacci(num: u32){
+fn fibonacci(num: u32) {
     let mut n1 = 0;
     let mut n2 = 1;
+    if num > 1 {
     println!("{n1}");
+    }
+    if num > 2 {
     println!("{n2}");
-    for _i in 1..num-1 {
-        let n3 = n1+n2;
+    }
+    for _i in 1..num - 1 {
+        let n3 = n1 + n2;
         println!("{n3}");
         n1 = n2;
         n2 = n3;
