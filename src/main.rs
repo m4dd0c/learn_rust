07f68daddd3cc495be4062ab34b6a159abcd8a2a
Manish Kumar <77256586+m4dd0c@users.noreply.github.com>
2026-04-22 07:26:21 +0000

    feat: user input added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    // asking for the number of elems from user
+    println!("Enter number of elements you need");
+    let mut num = String::new();
+    io::stdin().read_line(&mut num).expect("Expected string");
+    println!("num is {num}");
 }
