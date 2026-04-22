d1ed89282efc7626204beff2ad2acac3a5e81a42
Manish Kumar <77256586+m4dd0c@users.noreply.github.com>
2026-04-22 07:35:57 +0000

    feat: fibonacci series til n

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,21 @@ fn main() {
     println!("Enter number of elements you need");
     let mut num = String::new();
     io::stdin().read_line(&mut num).expect("Expected string");
-    println!("num is {num}");
+    let num: u32 = num.trim().parse().expect("Not a Number");
+    println!("Printing {num} Elements of fibonacci series...");
+    fibonacci(num);
+    println!("Program ended");
+}
+
+fn fibonacci(num: u32){
+    let mut n1 = 0;
+    let mut n2 = 1;
+    println!("{n1}");
+    println!("{n2}");
+    for _i in 1..num-1 {
+        let n3 = n1+n2;
+        println!("{n3}");
+        n1 = n2;
+        n2 = n3;
+    }
 }
