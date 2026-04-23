747dfd757654983fe9e9f92fff9cbc2ad6431904
Manish Kumar <77256586+m4dd0c@users.noreply.github.com>
2026-04-23 12:50:07 +0000

    feat: minigrep done; left bug, file not found

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::env;
+use crate::grep::exec;
 fn main () {
     let args: Vec<String> = env::args().collect();
-    dbg!(&args);
+    println!("{:?}", args);
     let config = Config::build(&args).expect("Must be valid config.");
     // searching for query in provided file_path
-    //exec(config);
+    exec(&config);
 }
 
 struct Config {
@@ -17,10 +18,45 @@ impl Config {
     fn build(args: &[String]) -> Result<Config, &str>{
         let query = args[1].clone();
         let path = args[2].clone();
-        let mut ignore_case = false;
-        if args[3] == "-i" {
-            ignore_case = true;
-        }
+        let ignore_case = args.get(3).map_or(false, |arg| arg == "-i");
         Ok(Config {query, path, ignore_case})
+    }
+}
+
+mod grep {
+    use super::Config;
+    use std::fs;
+
+    pub fn exec(conf: &Config) {
+        // reading the file
+        let content = read_file(&conf.path).expect("File should open.");
+        // calling search based on case sensi
+        if conf.ignore_case {
+            search_case_insensitive(&conf.query, &content);
+        }else {
+            search_case_sensitive(&conf.query, &content);
+        }
+    }
+    fn read_file(path: &String) -> Result<String, std::io::Error> {
+        let content = fs::read_to_string(path)?;
+        Ok(content)
+    }
+    fn search_case_sensitive(query: &String, content: &String) -> Vec<String> {
+        let mut results = Vec::new();
+        for line in content.lines() {
+            if line.contains(query){
+                results.push(line.to_string());
+            }
+        }
+        results
+    }
+    fn search_case_insensitive(query: &String, content: &String) -> Vec<String> {
+        let mut results = Vec::new();
+        for line in content.to_lowercase().lines() {
+            if line.contains(&query.to_lowercase()){
+                results.push(line.to_string());
+            }
+        }
+        results
     }
 }
--- src/utils/grep.rs
+++ src/utils/grep.rs
@@ -1,8 +1,34 @@
 mod grep {
     pub fn exec(conf: &Config) {
-        
+        // reading the file
+        let content = read_file(&conf.path).expect("File should open.");
+        // calling search based on case sensi
+        if conf.ignore_case {
+            search_case_insensitive(&conf.query, &content)
+        }else {
+            search_case_sensitive(&conf.query, &content)
+        }
     }
-    fn read_file() {}
-    fn search_case_sensitive() {}
-    fn search_case_insensitive() {}
+    fn read_file(path: &String) -> Vec<String> {
+        let content = fs::read_to_string(path)?;
+        Ok(content)
+    }
+    fn search_case_sensitive(query: String, content: String) {
+        let results = Vec::new();
+        for line in content.lines() {
+            if line.contains(query){
+                results.push(line);
+            }
+        }
+        results
+    }
+    fn search_case_insensitive(query: String, content: String) {
+        let results = Vec::new();
+        for line in content.lines().to_lowercase() {
+            if line.contains(query.to_lowercase()){
+                results.push(line);
+            }
+        }
+        results
+    }
 }
