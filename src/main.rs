9ab00ab4b574b5e845a5618cf24a7736ee771999
Manish Kumar <77256586+m4dd0c@users.noreply.github.com>
2026-04-23 12:28:48 +0000

    chore: scaffolding minigrep done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::env;
 fn main () {
-    println!("hello world");
+    let args: Vec<String> = env::args().collect();
+    dbg!(&args);
+    let config = Config::build(&args).expect("Must be valid config.");
+    // searching for query in provided file_path
+    //exec(config);
+}
+
+struct Config {
+    ignore_case: bool,
+    path: String,
+    query: String
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &str>{
+        let query = args[1].clone();
+        let path = args[2].clone();
+        let mut ignore_case = false;
+        if args[3] == "-i" {
+            ignore_case = true;
+        }
+        Ok(Config {query, path, ignore_case})
+    }
 }
---
+++ src/utils/grep.rs
@@ -0,0 +1,8 @@
+mod grep {
+    pub fn exec(conf: &Config) {
+        
+    }
+    fn read_file() {}
+    fn search_case_sensitive() {}
+    fn search_case_insensitive() {}
+}
